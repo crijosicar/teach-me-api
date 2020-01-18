256e77f1e59a5a2b64a525fae9e5238f3b53d234
crijosicar <12100677+crijosicar@users.noreply.github.com>
2020-01-18 16:29:20 -0500

    remove unnecessary imports

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { ROLE_MODEL, SUBJECT_MODEL, USER_MODEL } from '../constants';
+import { USER_MODEL } from '../constants';
 import { CreateUserDto } from './createUser.dto';
 import { User } from './user.interface';
 
